2ae526899f543d897f035fa06a5ca5b26e3a9e2f
ruslana-shev <ruslana.shevandina@gmail.com>
2017-01-19 19:34:29 +0200

    создан проект

---
+++ js/init.js
@@ -0,0 +1,17 @@
+/*init bpopup*/
+$('.open-order-modal').on('click', function () {
+    $('.modal-order').bPopup({
+        closeClass: '_close-popup',
+    });
+});
+/*init formstyle*/
+ $('select').styler();
+
+/*init slick*/
+$('.about-us-slick').slick({
+  dots: false,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+});
